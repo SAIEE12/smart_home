4f255714fb66794ea4ede6fb7b0c0f703361404d
cheelianil <cheelianil14@gmail.com>
2025-04-18 17:52:54 +0530

    changed in temp

--- src/temperature_control.c
+++ src/temperature_control.c
@@ -2,12 +2,15 @@
 #include "temperature_control.h"
 
 static int temperature = 22; // Default temperature 22°C
+int x;
 
 void set_temperature(int temp) {
     temperature = temp;
     printf("Temperature set to %d°C\n", temperature);
 }
-
+int x;
+int y;
 int get_temperature(void) {
     return temperature;
+
 }
